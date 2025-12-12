836ce7db98442dc73610e206f0851468f3aae4fb
NJ-Lighting <nick.jonker.tech@gmail.com>
2025-12-12 19:47:06 +0000

    d

--- js/supabase.client.js
+++ js/supabase.client.js
@@ -1,5 +1,5 @@
 // type: module
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2?target=browser';
 
 export const SUPABASE_URL = "https://stmpommlhkokcjkwivfc.supabase.co";
 export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0bXBvbW1saGtva2Nqa3dpdmZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzODA5MzMsImV4cCI6MjA1Njk1NjkzM30.U7MCLsJdc21aw8dhE9a0nvuuypgBeWL9feAqlaiXqOo";
